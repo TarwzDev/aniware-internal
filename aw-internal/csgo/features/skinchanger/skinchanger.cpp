bb1d7a52cbcb39b69b14b7449a1e048717482160
iBambooFox <stian.ostby@gmail.com>
2020-04-19 22:12:12 +0200

    removed skinchanger

--- aw-internal/csgo/features/skinchanger/skinchanger.cpp
+++ aw-internal/csgo/features/skinchanger/skinchanger.cpp
@@ -9,15 +9,5 @@ namespace skinchanger
 
 		if ( !ctx::client.local || !ctx::client.local->is_alive( ) )
 			return;
-
-		for ( auto i = 0; ctx::client.local->get_weapons( )[ i ]; i++ )
-		{
-			auto weapon = entity_t::get< weapon_t >( ctx::client.local->get_weapons( )[ i ] );	
-
-			if ( !weapon )
-				continue;
-
-			auto index = weapon->get_definition_index( );
-		}
 	}
 }
